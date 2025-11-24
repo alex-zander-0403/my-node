b33eeb8a38807175af2fd7d9d32a64d5b9ac2451
alex-zander-0403 <alexzander0403@gmail.com>
2025-11-24 09:00:39 +0300

    fs.rmdir

--- file-system.js
+++ file-system.js
@@ -6,7 +6,9 @@ const path = require("path");
 fs.mkdirSync(path.resolve(__dirname, "newDir"));
 
 // создание нескольких вложенных папок рекурсивно
-fs.mkdirSync(path.resolve(__dirname, "newDir1", "newDir2", "newDir3"), { recursive:true });
+fs.mkdirSync(path.resolve(__dirname, "newDir1", "newDir2", "newDir3"), {
+  recursive: true,
+});
 
 // создание папки асинхронно
 fs.mkdir(path.resolve(__dirname, "newDir"), (err) => {
@@ -15,4 +17,15 @@ fs.mkdir(path.resolve(__dirname, "newDir"), (err) => {
     return;
   }
   console.log("Папка создана");
+});
+
+// ============================================
+
+// удаление
+fs.rmdir(path.resolve(__dirname, "newDir"), (err) => {
+  if (err) {
+    console.log(err.message);
+    return;
+  }
+  console.log("Папка удалена");
 });
