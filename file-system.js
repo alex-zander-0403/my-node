7e3151f4ee736434e3b248fa8df90d1b2b8c9ed9
alex-zander-0403 <alexzander0403@gmail.com>
2025-11-24 08:35:38 +0300

    fs.mkdir

---
+++ file-system.js
@@ -0,0 +1,18 @@
+// require("dotenv").config();
+const fs = require("fs");
+const path = require("path");
+
+// создание папки
+fs.mkdirSync(path.resolve(__dirname, "newDir"));
+
+// создание нескольких вложенных папок рекурсивно
+fs.mkdirSync(path.resolve(__dirname, "newDir1", "newDir2", "newDir3"), { recursive:true });
+
+// создание папки асинхронно
+fs.mkdir(path.resolve(__dirname, "newDir"), (err) => {
+  if (err) {
+    console.log(err.message);
+    return;
+  }
+  console.log("Папка создана");
+});
